d1f6f0b4a4e41f22aa60a46d3f916ddd0a575443
lukezhangcj25 <2622874867@qq.com>
2020-03-31 15:56:58 +0800

    20200331

---
+++ src/main/java/com/info/sms/sms/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.info.sms.sms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Luke 2020/3/30 10:34
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
